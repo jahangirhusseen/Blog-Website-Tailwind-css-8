002b87e34ddb7bc1f06f05a362f0b00fb0486402
jahangirhusseen <jahangirhusseenid@gmail.com>
2023-12-07 19:29:54 +0600

    final project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['*'],
-  fontFamily: {
-    'poppins':['Poppins', 'sans-serif'],
-  },
   theme: {
     extend: {},
+    fontFamily: {
+      'poppins':['Poppins', 'sans-serif'],
+    },
   },
   plugins: [],
 }
